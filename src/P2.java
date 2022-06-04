07a9b5614393d2d7b122567a76638916854ee9ab
Safa <safa.umakhanov@outlook.com>
2022-06-04 21:59:23 +0400

    SOLVED2

---
+++ src/P2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int i = sc.nextInt();
+        String s = sc.nextLine();
+        double d =sc.nextDouble();
+        int c =sc.nextInt();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + c);
+    }
+
+    }
+
